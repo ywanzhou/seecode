867983d6425b81e5ed4e29dc66f7d4c740c6a921
Zhou-Zhonghua <pacpc@qq.com>
2022-07-08 11:01:02 +0800

    feat: study 验证管道参数

--- packages/serve/src/cats/cats.controller.ts
+++ packages/serve/src/cats/cats.controller.ts
@@ -4,18 +4,19 @@ import { HttpExceptionFilter } from './../http-exception.filter';
 import { CatsService } from './cats.service';
 import { CreateDto } from './dto/cat';
 import { Cat } from './interface/cat.interface';
+import { ValidationPipe } from './../validate.pipe';
 
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
   @Post()
-  async create(@Body() cat: CreateDto): Promise<void> {
+  async create(@Body(new ValidationPipe()) cat: CreateDto): Promise<void> {
     console.log(cat);
     this.catsService.create(cat);
   }
 
   @Get()
-  @UseFilters(HttpExceptionFilter) // 尽可能使用类而不是实例。由于 Nest 可以轻松地在整个模块中重复使用同一类的实例，因此可以减少内存使用。
+  @UseFilters(HttpExceptionFilter)
   async findAll(): Promise<Cat[]> {
     throw new ForbiddenException();
     // return this.catsService.findAll();
--- packages/serve/src/cats/dto/cat.ts
+++ packages/serve/src/cats/dto/cat.ts
@@ -1,5 +1,9 @@
+import { IsInt, IsString } from 'class-validator';
 export class CreateDto {
+  @IsString()
   readonly name: string;
+  @IsInt()
   readonly age: number;
+  @IsString()
   readonly breed: string;
 }
