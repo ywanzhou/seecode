f1d9626ba090f7cf14ed6aedf073d8447057796d
Zhou-Zhonghua <pacpc@qq.com>
2022-07-11 23:43:54 +0800

    feat: study guard

--- packages/serve/src/cats/cats.controller.ts
+++ packages/serve/src/cats/cats.controller.ts
@@ -1,17 +1,28 @@
-import { Controller, Get, Post, Body, UseFilters } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { ForbiddenException } from './../forbidden.exception';
 import { HttpExceptionFilter } from './../http-exception.filter';
 import { CatsService } from './cats.service';
 import { CreateDto } from './dto/cat';
 import { Cat } from './interface/cat.interface';
 import { ValidationPipe } from './../validate.pipe';
+import { AuthGuard } from './../roles.guard';
+import { Roles } from '../roles.decorator';
 
 @Controller('cats')
+@UseGuards(AuthGuard)
 export class CatsController {
   constructor(private catsService: CatsService) {}
+
   @Post()
+  @Roles('admin')
   async create(@Body(new ValidationPipe()) cat: CreateDto): Promise<void> {
-    console.log(cat);
     this.catsService.create(cat);
   }
 
---
+++ packages/serve/src/roles.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    // 假设 user 包含用户实例和允许的角色
+    const user = request.user;
+    console.log(roles, user, '000');
+
+    return true;
+  }
+}
