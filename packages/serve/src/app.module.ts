a197c6cad374190bd4b7c9c325a528ec8ed41649
Zhou-Zhonghua <pacpc@qq.com>
2022-07-13 23:29:36 +0800

    feat: 自带环境配置

--- packages/serve/src/app.module.ts
+++ packages/serve/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module, RequestMethod } from '@nestjs/common';
 import type { MiddlewareConsumer, NestModule } from '@nestjs/common';
+// 引入环境配置
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cats/cats.module';
@@ -7,7 +9,7 @@ import { LoggerMiddleware } from './middleware/logger.middleware';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CatModule, UserModule],
+  imports: [CatModule, UserModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
