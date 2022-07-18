b9f8741795611bda9b963b8998bc5fe20f5d84ed
ywanzhou <ywanzhou@foxmail.com>
2022-07-18 14:28:51 +0800

    feat(config): ✨ 添加local的配置文件

--- packages/serve/src/utils/yaml/index.ts
+++ packages/serve/src/utils/yaml/index.ts
@@ -1,10 +1,10 @@
 import { parse } from 'yaml';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+import { merge } from 'lodash';
 
 // 获取项目运行环境
 export const getEnv = () => {
-  console.log(process.env.RUNNING_ENV);
   return process.env.RUNNING_ENV;
 };
 
@@ -12,7 +12,19 @@ export const getEnv = () => {
 export const getConfig = () => {
   const environment = getEnv() || 'dev';
   const yamlPath = join(process.cwd(), `./.config/.${environment}.yaml`);
+  const yamlLocalPath = join(
+    process.cwd(),
+    `./.config/.${environment}.lo1cal.yaml`,
+  );
   const file = readFileSync(yamlPath, 'utf8');
-  const config = parse(file);
+
+  let fileLocal;
+  try {
+    fileLocal = readFileSync(yamlLocalPath, 'utf8');
+  } catch (error) {
+    console.warn('未添加local配置文件');
+  }
+  const config = merge({}, parse(file), fileLocal && parse(fileLocal));
+
   return config;
 };
